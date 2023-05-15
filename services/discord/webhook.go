922cf10573cd0e5e765e663355ef918a8ed8b238
TheAmniel <luismartz199@gmail.com>
2023-05-14 20:56:25 -0400

    Add base for future Discord logs with webhook

---
+++ services/discord/webhook.go
@@ -0,0 +1,26 @@
+package discord
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+// TODO
+type WebhookSend struct {
+	Content   string         `json:"content,omitempty"`
+	Username  string         `json:"username,omitempty"`
+	AvatarURL string         `json:"avatar_url,omitempty"`
+	Embeds    []any `json:"embeds,omitempty"`
+}
+
+func SendWebhook(data WebhookSend) error {
+	body, err := json.Marshal(data)
+	var resp *http.Response
+
+	resp, err = http.Post("", "application/json", bytes.NewReader(body))
+	bodyBytes, err := io.ReadAll(resp.Body)
+	print(string(bodyBytes))
+	return err
+}
